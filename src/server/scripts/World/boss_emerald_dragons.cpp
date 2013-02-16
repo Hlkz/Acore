7eb0e122296a25088293881c635c89bdf9068543
Hlkz <Hlkz@hotmail.fr>
2013-02-16 16:24:09 +0100

    Marque de la nature

--- src/server/scripts/World/boss_emerald_dragons.cpp
+++ src/server/scripts/World/boss_emerald_dragons.cpp
@@ -108,8 +108,8 @@ struct emerald_dragonAI : public WorldBossAI
     // Target killed during encounter, mark them as suspectible for Aura Of Nature
     void KilledUnit(Unit* who)
     {
-        if (who->GetTypeId() == TYPEID_PLAYER)
-            who->CastSpell(who, SPELL_MARK_OF_NATURE, true);
+        //if (who->GetTypeId() == TYPEID_PLAYER)
+        //    who->CastSpell(who, SPELL_MARK_OF_NATURE, true);
     }
 
     // Execute and reschedule base events shared between all Emerald Dragons
@@ -719,8 +719,8 @@ class MarkOfNatureTargetSelector
         bool operator()(WorldObject* object)
         {
             // return those not tagged or already under the influence of Aura of Nature
-            //if (Unit* unit = object->ToUnit())
-            //    return !(unit->HasAura(SPELL_MARK_OF_NATURE) && !unit->HasAura(SPELL_AURA_OF_NATURE));
+            if (Unit* unit = object->ToUnit())
+                return !(unit->HasAura(SPELL_MARK_OF_NATURE) && !unit->HasAura(SPELL_AURA_OF_NATURE));
             return true;
         }
 };
