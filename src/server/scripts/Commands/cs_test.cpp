ab997218028d9fa81a5270b19261f8c19e5b0e69
Hlkz <Hlkz@hotmail.fr>
2013-04-22 16:28:45 +0200

    clean test command

--- src/server/scripts/Commands/cs_test.cpp
+++ src/server/scripts/Commands/cs_test.cpp
@@ -7,10 +7,6 @@ class com_test : public CommandScript {
 
 static bool HandleComTest(ChatHandler* handler, char const* /*args*/) {
 	Player* target = handler->getSelectedPlayer();
-	sLog->outError(LOG_FILTER_GENERAL, "team : %u", target->GetTeam());
-	sLog->outError(LOG_FILTER_GENERAL, "ffa? : %u", target->HasByteFlag(UNIT_FIELD_BYTES_2, 1, UNIT_BYTE2_FLAG_FFA_PVP));
-    target->SetByteFlag(UNIT_FIELD_BYTES_2, 1, 0x01);
-
 	return true; }
 
 ChatCommand* GetCommands() const {
