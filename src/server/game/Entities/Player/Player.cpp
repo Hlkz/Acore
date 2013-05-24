d1820704fb88ea63732ff1ebc7d7a8eff906b46a
Shauren <shauren.trinity@gmail.com>
2013-05-24 20:06:58 +0200

    Scripts/Halls of Reflection: Fixed gauntlet event
    Conflicts:
    	src/server/game/Entities/Player/Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -9650,6 +9650,16 @@ void Player::SendInitWorldStates(uint32 zoneid, uint32 areaid)
         case 5810:
             sBattleAOMgr->GetBattleAO()->FillInitialWorldStates(data);
             break;
+        case 4820:
+            if (instance && mapid == 668)
+                instance->FillInitialWorldStates(data);
+            else
+            {
+                data << uint32(4884) << uint32(0);              // 9  WORLD_STATE_HOR_WAVES_ENABLED
+                data << uint32(4882) << uint32(0);              // 10 WORLD_STATE_HOR_WAVE_COUNT
+            }
+            break;
+
             // No break here, intended.
         default:
             data << uint32(0x914) << uint32(0x0);           // 7
