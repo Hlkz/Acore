1bb992a0987c2c656c83853888a8d85b69273cb3
Shauren <shauren.trinity@gmail.com>
2013-05-28 15:19:37 +0200

    Core/Players: Send only values update when turning gm mode on or accepting quests instead of recreating the object clientside

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22264,7 +22264,7 @@ void Player::UpdateTriggerVisibility()
             if (!obj || !(obj->isTrigger() || obj->HasAuraType(SPELL_AURA_TRANSFORM)))  // can transform into triggers
                 continue;
 
-            obj->BuildCreateUpdateBlockForPlayer(&udata, this);
+            obj->BuildValuesUpdateBlockForPlayer(&udata, this);
         }
     }
 
@@ -23251,7 +23251,7 @@ void Player::UpdateForQuestWorldObjects()
 
                 if (buildUpdateBlock)
                 {
-                    obj->BuildCreateUpdateBlockForPlayer(&udata, this);
+                    obj->BuildValuesUpdateBlockForPlayer(&udata, this);
                     break;
                 }
             }
