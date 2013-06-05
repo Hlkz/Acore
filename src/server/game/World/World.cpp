210caffbf610483299d985312562ba2fd8cdca21
corbi <alexei_93@yahoo.com>
2013-06-05 10:34:05 +0300

    Stats Limits
    Conflicts:
    	src/server/game/World/World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -1129,6 +1129,13 @@ void World::LoadConfigSettings(bool reload)
 
     m_bool_configs[CONFIG_ARENA_SPECTATORS_ENABLE] = ConfigMgr::GetBoolDefault("Arena.Spectators.Enable", false);
     
+    // Stats limits
+    m_bool_configs[CONFIG_STATS_LIMITS_ENABLE] = ConfigMgr::GetBoolDefault("Stats.Limits.Enable", false);
+    m_float_configs[CONFIG_STATS_LIMITS_DODGE] = ConfigMgr::GetFloatDefault("Stats.Limits.Dodge", 95.0f);
+    m_float_configs[CONFIG_STATS_LIMITS_PARRY] = ConfigMgr::GetFloatDefault("Stats.Limits.Parry", 95.0f);
+    m_float_configs[CONFIG_STATS_LIMITS_BLOCK] = ConfigMgr::GetFloatDefault("Stats.Limits.Block", 95.0f);
+    m_float_configs[CONFIG_STATS_LIMITS_CRIT] = ConfigMgr::GetFloatDefault("Stats.Limits.Crit", 95.0f);
+
     // call ScriptMgr if we're reloading the configuration
     
     if (reload)
