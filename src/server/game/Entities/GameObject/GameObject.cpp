0d54e747e434afd550c0db9bb846fa756e4bed56
Hlkz <Hlkz@hotmail.fr>
2013-07-14 22:28:57 +0200

    Revert "Core/Collision: Fixed LoS for doors with data0 = 0 and state = 0"
    This reverts commit b0bcf1f67caee3adac49d46ddb00af986e6e27cd.
    
    Conflicts:
    	src/server/game/Entities/GameObject/GameObject.cpp

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -136,7 +136,6 @@ void GameObject::AddToWorld()
 
         sObjectAccessor->AddObject(this);
         bool startOpen = (GetGoType() == GAMEOBJECT_TYPE_DOOR || GetGoType() == GAMEOBJECT_TYPE_BUTTON ? GetGOInfo()->door.startOpen : false);
-        bool toggledState = (GetGOData() ? GetGOData()->go_state == GO_STATE_ACTIVE : false);
         bool toggledState = GetGoType() == GAMEOBJECT_TYPE_CHEST ? getLootState() == GO_READY : GetGoState() == GO_STATE_READY;
         if (m_model)
             GetMap()->InsertGameObjectModel(*m_model);
