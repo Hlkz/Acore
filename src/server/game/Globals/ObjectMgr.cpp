f3c7d4f74231306b9caea7e69d24c691bec7ca16
joschiwald <joschiwald@online.de>
2013-07-21 13:37:34 +0200

    Core/Spells: use SpellInfo class to get spell rank instead of SpellMgr helpers
    Conflicts:
    	src/server/scripts/Spells/spell_paladin.cpp

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -4908,7 +4908,7 @@ void ObjectMgr::LoadSpellScriptNames()
             while (spellInfo)
             {
                 _spellScriptsStore.insert(SpellScriptsContainer::value_type(spellInfo->Id, GetScriptId(scriptName)));
-                spellInfo = sSpellMgr->GetSpellInfo(spellInfo->Id)->GetNextRankSpell();
+                spellInfo = spellInfo->GetNextRankSpell();
             }
         }
         else
