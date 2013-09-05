f3e53fe67ce8d0afe8a862fd30c52e7f2b7ae1a2
Hlkz <Hlkz@hotmail.fr>
2013-09-06 00:55:55 +0200

    conflits

--- src/server/worldserver/RemoteAccess/RASocket.h
+++ src/server/worldserver/RemoteAccess/RASocket.h
@@ -37,9 +37,9 @@ class RASocket : public ACE_Svc_Handler<ACE_SOCK_STREAM, ACE_MT_SYNCH>
         RASocket();
         virtual ~RASocket() { }
 
-        virtual int svc() OVERRIDE;
-        virtual int open(void* = 0) OVERRIDE;
-        virtual int handle_close(ACE_HANDLE = ACE_INVALID_HANDLE, ACE_Reactor_Mask = ACE_Event_Handler::ALL_EVENTS_MASK) OVERRIDE;
+        virtual int svc();
+        virtual int open(void* = 0);
+        virtual int handle_close(ACE_HANDLE = ACE_INVALID_HANDLE, ACE_Reactor_Mask = ACE_Event_Handler::ALL_EVENTS_MASK);
 
     private:
         int recv_line(std::string& outLine);
