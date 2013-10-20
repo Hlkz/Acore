f436280c74ae53b4f9fba4a75db63c8eb7e6999e
Shauren <shauren.trinity@gmail.com>
2013-10-20 14:27:17 +0200

    Core/Transports: Fixed crashes happening when passenger is removed from transport during teleport
    Closes #11067
    
    Conflicts:
    	src/server/game/Entities/Transport/Transport.cpp

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -101,7 +101,11 @@ void GameObject::CleanupsBeforeDelete(bool /*finalCleanup*/)
         RemoveFromOwner();
 
     if (GetTransport() && !ToTransport())
+    {
         GetTransport()->RemovePassenger(this);
+        SetTransport(NULL);
+        m_movementInfo.transport.Reset();
+    }
 }
 
 void GameObject::RemoveFromOwner()
