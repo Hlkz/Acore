f4627873b236a6a4b911c312e98872869c3b41d2
jackpoz <giacomopoz@gmail.com>
2013-10-21 23:41:31 +0200

    Core/Quest: Remove assertion
    Remove assertion about Quest ExclusiveGroup since they are triggered even when adding valid quests to npcs. Fixes #8336 .
    How to reproduce steps:
    - .gm off
    - add quest 11335 in creature_queststarter
    - .reload creature_queststarter
    - get in range of that creature
    - .gm on
    - assertion triggered

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -15357,9 +15357,6 @@ bool Player::SatisfyQuestPreviousQuest(Quest const* qInfo, bool msg)
                 // can be start if only all quests in prev quest exclusive group completed and rewarded
                 ObjectMgr::ExclusiveQuestGroupsBounds range(sObjectMgr->mExclusiveQuestGroups.equal_range(qPrevInfo->GetExclusiveGroup()));
 
-                // always must be found if qPrevInfo->ExclusiveGroup != 0
-                ASSERT(range.first != range.second);
-
                 for (; range.first != range.second; ++range.first)
                 {
                     uint32 exclude_Id = range.first->second;
@@ -15392,9 +15389,6 @@ bool Player::SatisfyQuestPreviousQuest(Quest const* qInfo, bool msg)
                 // each-from-all exclusive group (< 0)
                 // can be start if only all quests in prev quest exclusive group active
                 ObjectMgr::ExclusiveQuestGroupsBounds range(sObjectMgr->mExclusiveQuestGroups.equal_range(qPrevInfo->GetExclusiveGroup()));
-
-                // always must be found if qPrevInfo->ExclusiveGroup != 0
-                ASSERT(range.first != range.second);
 
                 for (; range.first != range.second; ++range.first)
                 {
@@ -15562,9 +15556,6 @@ bool Player::SatisfyQuestExclusiveGroup(Quest const* qInfo, bool msg)
         return true;
 
     ObjectMgr::ExclusiveQuestGroupsBounds range(sObjectMgr->mExclusiveQuestGroups.equal_range(qInfo->GetExclusiveGroup()));
-
-    // always must be found if qInfo->ExclusiveGroup != 0
-    ASSERT(range.first != range.second);
 
     for (; range.first != range.second; ++range.first)
     {
