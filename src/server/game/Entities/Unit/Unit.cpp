f3d649940da4fe20d73a4c342574a353b162985b
Shauren <shauren.trinity@gmail.com>
2013-10-22 17:09:41 +0200

    Core/Auras: Added serverside checking for UNIT_FLAG_PACIFIED when attempting to start melee attack
    Closes #8623

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8902,6 +8902,9 @@ bool Unit::Attack(Unit* victim, bool meleeAttack)
     if (GetTypeId() == TYPEID_PLAYER && IsMounted())
         return false;
 
+    if (HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PACIFIED))
+        return false;
+
     // nobody can attack GM in GM-mode
     if (victim->GetTypeId() == TYPEID_PLAYER)
     {
