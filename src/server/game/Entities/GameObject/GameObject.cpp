e242a9ed46eda328aa30b9608dff075eef27e7d8
jackpoz <giacomopoz@gmail.com>
2014-03-24 20:13:55 +0100

    Core/Misc: Fix assertion triggered
    Change an assertion added in 6dcd8c8545a65a7bfbd0daaa16f650d8c3a90262 with a NULL check, properly handling the case of a NULL parameter.
    Fixes #11771

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1016,7 +1016,6 @@ void GameObject::TriggeringLinkedGameObject(uint32 trapEntry, Unit* target)
     if (!trapSpell)                                          // checked at load already
         return;
 
-    ASSERT(GetOwner());
     float range = float(target->GetSpellMaxRangeForTarget(GetOwner(), trapSpell));
 
     // search nearest linked GO
