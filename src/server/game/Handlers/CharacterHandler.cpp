6bd0858b50db62250fc82573af401037dd436238
jackpoz <giacomopoz@gmail.com>
2014-04-20 14:58:24 +0200

    Core/Misc: Fix change faction on dead Night Elves keeping the speed buff
    Fix change faction on dead Night Elves causing the new player keeping the 175% wisp speed buff.
    Players will be resurrected automatically on change faction.
    Closes #255

--- src/server/game/Handlers/CharacterHandler.cpp
+++ src/server/game/Handlers/CharacterHandler.cpp
@@ -1741,6 +1741,9 @@ void WorldSession::HandleCharFactionOrRaceChange(WorldPacket& recvData)
         }
     }
 
+    // resurrect the character in case he's dead
+    sObjectAccessor->ConvertCorpseForPlayer(guid);
+
     CharacterDatabase.EscapeString(newname);
     Player::Customize(guid, gender, skin, face, hairStyle, hairColor, facialHair);
     SQLTransaction trans = CharacterDatabase.BeginTransaction();
