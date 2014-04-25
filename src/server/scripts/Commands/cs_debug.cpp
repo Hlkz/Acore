b674b428e0146fdf148c9601884638a784e39719
jackpoz <giacomopoz@gmail.com>
2014-04-25 21:19:30 +0200

    Core/Misc: Change how Position struct is retrieved
    Update Position::GetPosition() and similar methods signatures to reflect 2a4c9bcaf910430cdf3070987ce085da3c2666da changes by return a Position object instead of accepting a Position parameter by reference.
    
    Conflicts:
    	src/server/game/Battlefield/Zones/BattlefieldWG.cpp
    	src/server/game/Battlefield/Zones/BattlefieldWG.h
    	src/server/game/Movement/MovementGenerators/FleeingMovementGenerator.cpp
    	src/server/scripts/Commands/cs_debug.cpp
    	src/server/scripts/EasternKingdoms/Karazhan/boss_prince_malchezaar.cpp
    	src/server/scripts/EasternKingdoms/Karazhan/bosses_opera.cpp
    	src/server/scripts/Kalimdor/CavernsOfTime/BattleForMountHyjal/boss_archimonde.cpp
    	src/server/scripts/Kalimdor/CavernsOfTime/TheBlackMorass/instance_the_black_morass.cpp
    	src/server/scripts/Kalimdor/CavernsOfTime/TheBlackMorass/the_black_morass.cpp
    	src/server/scripts/Kalimdor/OnyxiasLair/instance_onyxias_lair.cpp
    	src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_ayamiss.cpp
    	src/server/scripts/Kalimdor/RuinsOfAhnQiraj/boss_ossirian.cpp
    	src/server/scripts/Northrend/AzjolNerub/Ahnkahet/boss_amanitar.cpp
    	src/server/scripts/Northrend/AzjolNerub/AzjolNerub/boss_anubarak.cpp
    	src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
    	src/server/scripts/Northrend/Gundrak/boss_drakkari_colossus.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_lady_deathwhisper.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_lord_marrowgar.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_professor_putricide.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
    	src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel.cpp
    	src/server/scripts/Northrend/Nexus/EyeOfEternity/boss_malygos.cpp
    	src/server/scripts/Northrend/Nexus/EyeOfEternity/instance_eye_of_eternity.cpp
    	src/server/scripts/Northrend/Nexus/Oculus/boss_drakos.cpp
    	src/server/scripts/Northrend/Nexus/Oculus/boss_urom.cpp
    	src/server/scripts/Northrend/Nexus/Oculus/boss_varos.cpp
    	src/server/scripts/Northrend/Nexus/Oculus/oculus.cpp
    	src/server/scripts/Northrend/Ulduar/HallsOfLightning/boss_ionar.cpp
    	src/server/scripts/Northrend/Ulduar/Ulduar/boss_freya.cpp
    	src/server/scripts/Northrend/Ulduar/Ulduar/boss_kologarn.cpp
    	src/server/scripts/Northrend/Ulduar/Ulduar/boss_xt002.cpp
    	src/server/scripts/Northrend/zone_borean_tundra.cpp
    	src/server/scripts/Northrend/zone_wintergrasp.cpp
    	src/server/scripts/Outland/TempestKeep/Mechanar/boss_mechano_lord_capacitus.cpp
    	src/server/scripts/Outland/zone_shadowmoon_valley.cpp
    	src/server/scripts/World/boss_emerald_dragons.cpp

--- src/server/scripts/Commands/cs_debug.cpp
+++ src/server/scripts/Commands/cs_debug.cpp
@@ -1374,7 +1374,9 @@ public:
         }
         else
         {
+            Position pos = transport->GetPosition();
             handler->PSendSysMessage("Transport %s is %s", transport->GetName().c_str(), transport->GetGoState() == GO_STATE_READY ? "stopped" : "moving");
+            handler->PSendSysMessage("Transport position: %s", pos.ToString().c_str());
             return true;
         }
 
