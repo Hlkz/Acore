9b495ad6064e28579bb364df73f18aab40901178
MitchesD <majklprofik@seznam.cz>
2014-05-23 20:51:22 +0200

    Scripts/Commands: added option to show InhabitType in .npc info command

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1216,6 +1216,8 @@ enum TrinityStrings
     LANG_BG_BA_NEXUS_VULN_A             = 18006,
     LANG_BG_BA_NEXUS_VULN_H             = 18007,
 
+    LANG_NPCINFO_INHABIT_TYPE                     = 11008
+
     // NOT RESERVED IDS                   12000-1999999999
     // `db_script_string` table index     2000000000-2000009999 (MIN_DB_SCRIPT_STRING_ID-MAX_DB_SCRIPT_STRING_ID)
     // For other tables maybe             2000010000-2147483647 (max index)
