7e5d73dc46871389048077a01d53eba0f3d730e5
jackpoz <giacomopoz@gmail.com>
2014-06-01 16:15:27 +0200

    Core/Misc: Code cleanup
    Remove old unused opcode handler WorldSession::HandlePageQuerySkippedOpcode() for CMSG_PAGE_TEXT_QUERY since it's already handled by WorldSession::HandlePageTextQueryOpcode()

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -720,7 +720,6 @@ class WorldSession
         void HandleCompleteCinematic(WorldPacket& recvPacket);
         void HandleNextCinematicCamera(WorldPacket& recvPacket);
 
-        void HandlePageQuerySkippedOpcode(WorldPacket& recvPacket);
         void HandlePageTextQueryOpcode(WorldPacket& recvPacket);
 
         void HandleTutorialFlag (WorldPacket& recvData);
