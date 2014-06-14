ddb8be243a1d58c55646bdb5234e22ace72f7b54
Vincent-Michael <Vincent_Michael@gmx.de>
2014-06-14 22:01:50 +0200

    Core/Ulduar: Added IsInCombat check for ulduar teleporter
    Conflicts:
    	src/server/game/AI/SmartScripts/SmartScript.cpp
    	src/server/game/Entities/GameObject/GameObject.h
    	src/server/scripts/Northrend/Ulduar/Ulduar/instance_ulduar.cpp

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -1302,7 +1302,7 @@ void GameObject::Use(Unit* user)
                 }
                 else if (info->goober.gossipID)
                 {
-                    player->PrepareGossipMenu(this, info->goober.gossipID);
+                    player->PrepareGossipMenu(this, info->goober.gossipID, true);
                     player->SendPreparedGossip(this);
                 }
 
@@ -1737,7 +1737,7 @@ void GameObject::Use(Unit* user)
         CastSpell(user, spellId);
 }
 
-void GameObject::CastSpell(Unit* target, uint32 spellId)
+void GameObject::CastSpell(Unit* target, uint32 spellId, bool triggered /*= true*/)
 {
     SpellInfo const* spellInfo = sSpellMgr->GetSpellInfo(spellId);
     if (!spellInfo)
@@ -1756,7 +1756,7 @@ void GameObject::CastSpell(Unit* target, uint32 spellId)
     if (self)
     {
         if (target)
-            target->CastSpell(target, spellInfo, true);
+            target->CastSpell(target, spellInfo, triggered);
         return;
     }
 
@@ -1770,14 +1770,14 @@ void GameObject::CastSpell(Unit* target, uint32 spellId)
         trigger->setFaction(owner->getFaction());
         // needed for GO casts for proper target validation checks
         trigger->SetOwnerGUID(owner->GetGUID());
-        trigger->CastSpell(target ? target : trigger, spellInfo, true, 0, 0, owner->GetGUID());
+        trigger->CastSpell(target ? target : trigger, spellInfo, triggered, 0, 0, owner->GetGUID());
     }
     else
     {
         trigger->setFaction(14);
         // Set owner guid for target if no owner available - needed by trigger auras
         // - trigger gets despawned and there's no caster avalible (see AuraEffect::TriggerSpell())
-        trigger->CastSpell(target ? target : trigger, spellInfo, true, 0, 0, target ? target->GetGUID() : 0);
+        trigger->CastSpell(target ? target : trigger, spellInfo, triggered, 0, 0, target ? target->GetGUID() : 0);
     }
 }
 
--- src/server/game/Entities/GameObject/GameObject.h
+++ src/server/game/Entities/GameObject/GameObject.h
@@ -788,7 +788,7 @@ public:
 
     GameObject* LookupFishingHoleAround(float range);
 
-    void CastSpell(Unit* target, uint32 spell);
+    void CastSpell(Unit* target, uint32 spell, bool triggered = true);
     void SendCustomAnim(uint32 anim);
     bool IsInRange(float x, float y, float z, float radius) const;
 
