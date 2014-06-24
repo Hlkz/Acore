1e1e2cc08723a61283f8c9dc1fb33a7361082e98
Subv <subv2112@gmail.com>
2014-06-24 13:17:23 -0500

    Core/Databases: Removed ACE dependencies on some of the database handling code.

--- src/server/game/World/World.h
+++ src/server/game/World/World.h
@@ -787,7 +787,7 @@ class World
         void LoadCharacterNameData();
 
         void ProcessQueryCallbacks();
-        ACE_Future_Set<PreparedQueryResult> m_realmCharCallbacks;
+        std::deque<std::future<PreparedQueryResult>> m_realmCharCallbacks;
 };
 
 extern uint32 realmID;
