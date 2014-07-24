750258e1f63c96e019daf8d2d20999df15572fe5
Trisjdc <trisjdc@gmail.com>
2014-07-24 04:06:49 +0100

    Core/Units: Properly remove auras that don't pass IsHighestExclusiveAura

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -3391,7 +3391,7 @@ void Unit::_RemoveNoStackAurasDueToAura(Aura* aura)
                 Spell::SendCastResult(caster->ToPlayer(), aura->GetSpellInfo(), 1, SPELL_FAILED_AURA_BOUNCED);
         }
 
-        RemoveAura(aura);
+        aura->Remove();
         return;
     }
 
