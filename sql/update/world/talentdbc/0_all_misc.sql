44be4894fa31402886f124ea537c77bfdde4bd96
Hlkz <hlkz@hotmail.fr>
2014-08-03 13:05:21 +0200

    SQL World Update - Talentdbc Pet Hunter

--- sql/update/world/talentdbc/0_all_misc.sql
+++ sql/update/world/talentdbc/0_all_misc.sql
@@ -1,2 +1,12 @@
 
-DELETE FROM `talentdbc` WHERE `TalentTab` < 401; -- Remove all talents exept for pets
+-- TalentID
+--
+-- 100+  200+  300+  : Paladin
+-- 400+  500+  600+  : Hunter
+-- 700+  800+  900+  : Rogue
+-- 1000+ 1100+ 1200+ : Warrior
+-- 1300+ 1400+ 1500+ : Mage
+--
+-- 9000+ 9100+ 9200+ : Pet Hunter
+
+DELETE FROM `talentdbc`;
