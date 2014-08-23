f82c9fbd74dc8bcafc6eea5f49a891322b0104c1
Hlkz <hlkz@hotmail.fr>
2014-08-23 17:53:41 +0200

    SQL World - factiondbc factiontemplatedbc
    + SQL World Update - factiondbc factiontemplatedbc

---
+++ sql/update/world/factiontemplatedbc.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO `factiontemplatedbc` VALUES ('3800', '3800', '1', '8', '0', '1', '0', '0', '0', '50', '3800', '0', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3801', '3801', '6177', '0', '0', '0', '3802', '0', '0', '0', '3801', '0', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3802', '3802', '6177', '0', '0', '0', '3801', '0', '0', '0', '3802', '0', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3804', '3804', '2081', '3', '2', '12', '3806', '67', '50', '0', '3804', '469', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3805', '3804', '0', '2', '2', '4', '0', '0', '0', '0', '3804', '469', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3806', '3806', '2081', '3', '2', '12', '3804', '469', '50', '0', '3806', '67', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3807', '3806', '0', '2', '2', '4', '0', '0', '0', '0', '3806', '67', '0', '0');
+INSERT INTO `factiontemplatedbc` VALUES ('3808', '3800', '1', '8', '0', '1', '50', '0', '0', '0', '3800', '0', '0', '0');
