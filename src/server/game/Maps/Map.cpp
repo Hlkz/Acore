92404db6ebd4ca91f6be7cfb697d5903111f85be
jackpoz <giacomopoz@gmail.com>
2014-10-12 20:01:27 +0200

    Core/Maps: Apply const-correctness to GridMap::loadData()

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -1591,7 +1591,7 @@ GridMap::~GridMap()
     unloadData();
 }
 
-bool GridMap::loadData(char* filename)
+bool GridMap::loadData(const char* filename)
 {
     // Unload old data if exist
     unloadData();
