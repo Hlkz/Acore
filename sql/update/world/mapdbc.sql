1aeb8f783da84b082f3e89b6af920aee083de5fd
Hlkz <hlkz@hotmail.fr>
2015-01-18 14:55:42 +0100

    SQL Update - Add some custom maps to Map table
    bao bao0 ba bb iwp

--- sql/update/world/mapdbc.sql
+++ sql/update/world/mapdbc.sql
@@ -1,2 +1,6 @@
 
-INSERT INTO `mapdbc` VALUES ('782', 'bao', '0', '0', '0', 'Limbo', 'Limbes', '5810', '', '', '', '', '144', '1', '0', '0', '0', '-1', '0', '0', '0');
+INSERT INTO `mapdbc` VALUES ('782', 'bao', '0', '0', '0', 'Limbo', 'Limbes', '5810', '', '', '', '', '7820', '1', '0', '0', '0', '-1', '0', '0', '0');
+INSERT INTO `mapdbc` VALUES ('783', 'bao0', '0', '0', '0', 'Limbo', 'Limbes', '5810', '', '', '', '', '7830', '1', '0', '0', '0', '-1', '0', '0', '0');
+INSERT INTO `mapdbc` VALUES ('784', 'ba', '3', '0', '1', 'Old Silvermoon', 'Ancienne Lune d\'Argent', '7840', '', '', '', '', '224', '1', '0', '0', '0', '-1', '0', '0', '0');
+INSERT INTO `mapdbc` VALUES ('785', 'bb', '3', '0', '1', 'Old Silvermoon', 'Ancienne Lune d\'Argent', '7840', '', '', '', '', '224', '1', '0', '0', '0', '-1', '0', '0', '0');
+INSERT INTO `mapdbc` VALUES ('786', 'iwp', '3', '0', '1', 'Strom', 'Strom', '7860', '', '', '', '', '23', '1', '0', '0', '0', '-1', '0', '0', '0');
