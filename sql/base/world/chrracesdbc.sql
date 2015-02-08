6c9ef964e4fdf1091154bae196b6d2c025ee70c5
Hlkz <hlkz@hotmail.fr>
2015-02-08 14:16:45 +0100

    SQL dbc - Import creaturedisplayinfo/extra/modeldata from MoP (5.4.2)
    + Update ChrRaces (adapt with worgens/pandarens)

--- sql/base/world/chrracesdbc.sql
+++ sql/base/world/chrracesdbc.sql
@@ -62,9 +62,10 @@ LOCK TABLES `chrracesdbc` WRITE;
 UNLOCK TABLES;
 
 --
--- Records of `chrracesdbc`
+-- Records of `chrracesdbc` from 3.3.5
 --
 
+/*
 INSERT INTO `chrracesdbc` VALUES ('1', '12', '1', '4140', '49', '50', 'Hu', '7', '7', '15007', '1096', 'Human', '81', '0', 'Human', 'Humain', 'Human', 'Humaine', 'Human', 'Humain', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
 INSERT INTO `chrracesdbc` VALUES ('2', '12', '2', '4141', '51', '52', 'Or', '1', '7', '15007', '1096', 'Orc', '21', '1', 'Orc', 'Orc', 'Orc', 'Orque', 'Orc', 'Orc', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
 INSERT INTO `chrracesdbc` VALUES ('3', '12', '3', '4147', '53', '54', 'Dw', '7', '7', '15007', '1090', 'Dwarf', '41', '0', 'Dwarf', 'Nain', 'Dwarf', 'Naine', 'Dwarf', 'Nain', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
@@ -86,3 +87,40 @@ INSERT INTO `chrracesdbc` VALUES ('18', '15', '1', '0', '21963', '21964', 'Ft', 
 INSERT INTO `chrracesdbc` VALUES ('19', '5', '1', '0', '26316', '26317', 'Wt', '7', '7', '15007', '1096', 'Taunka', '0', '2', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Normal', 'Normal', 'Normal', '0');
 INSERT INTO `chrracesdbc` VALUES ('20', '5', '1', '0', '26871', '26872', 'NS', '7', '7', '15007', '1096', 'NorthrendSkeleton', '0', '2', 'Northrend Skeleton', 'Squelette du Norfendre', 'Northrend Skeleton', 'Squelette du Norfendre', 'Northrend Skeleton', 'Squelette du Norfendre', 'Normal', 'Normal', 'Normal', '0');
 INSERT INTO `chrracesdbc` VALUES ('21', '5', '1', '0', '26873', '26874', 'It', '7', '7', '15007', '1096', 'IceTroll', '0', '2', 'Ice Troll', 'Troll des glaces', 'Ice Troll', 'Trollesse des glaces', 'Ice Troll', 'Troll des glaces', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('22','214','2203','20683','29422','29423','Wo','7','7','15007','1096','Worgen','170','0','Worgen','Worgen','Worgen','Worgen','Worgen','Worgen','NORMAL','EARS','NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('23','109','1','4140','49','50','Hu','7','7','15007','1096','Human','0','0','Gilnean','Gilnéen','Gilnean','Gilnéenne','Gilnean','Gilnéen','NORMAL','PIERCINGS','NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('24','120852','2395','4140','38551','38552','Pa','42','7','15007','1096','Pandaren','259','2','Pandaren','Pandaren','Pandaren','Pandarène','Pandaren','Pandaren','NORMAL','EARRINGS','NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('25','80388','2401','4140','38551','38552','Pa','7','7','15007','1096','Pandaren','259','0','Pandaren','Pandaren','Pandaren','Pandarène','Pandaren','Pandaren','NORMAL','EARRINGS','NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('26','80388','2402','4140','38551','38552','Pa','1','7','15007','1096','Pandaren','259','1','Pandaren','Pandaren','Pandaren','Pandarène','Pandaren','Pandaren','NORMAL','EARRINGS','NORMAL', '0');
+*/
+
+--
+-- Records of `chrracesdbc` for Aviana
+--
+
+INSERT INTO `chrracesdbc` VALUES ('1', '12', '1', '4140', '49', '50', 'Hu', '7', '7', '15007', '1096', 'Human', '81', '0', 'Human', 'Humain', 'Human', 'Humaine', 'Human', 'Humain', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('2', '12', '2', '4141', '51', '52', 'Or', '1', '7', '15007', '1096', 'Orc', '21', '1', 'Orc', 'Orc', 'Orc', 'Orque', 'Orc', 'Orc', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('3', '12', '1', '4147', '53', '54', 'Dw', '7', '7', '15007', '1090', 'Dwarf', '41', '0', 'Dwarf', 'Nain', 'Dwarf', 'Naine', 'Dwarf', 'Nain', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('4', '12', '1', '4145', '55', '56', 'Ni', '7', '7', '15007', '1096', 'NightElf', '61', '0', 'Night Elf', 'Elfe de la nuit', 'Night Elf', 'Elfe de la nuit', 'Night Elf', 'Elfe de la nuit', 'NORMAL', 'MARKINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('5', '12', '2', '4142', '57', '58', 'Sc', '1', '7', '15007', '1096', 'Scourge', '2', '1', 'Undead', 'Mort-vivant', 'Undead', 'Morte-vivante', 'Undead', 'Mort-vivant', 'FEATURES', 'FEATURES', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('6', '14', '2', '4143', '59', '60', 'Ta', '1', '7', '15007', '1096', 'Tauren', '141', '1', 'Tauren', 'Tauren', 'Tauren', 'Taurène', 'Tauren', 'Tauren', 'NORMAL', 'HAIR', 'HORNS', '0');
+INSERT INTO `chrracesdbc` VALUES ('7', '12', '1', '4146', '1563', '1564', 'Gn', '7', '7', '15007', '1096', 'Gnome', '101', '0', 'Gnome', 'Gnome', 'Gnome', 'Gnome', 'Gnome', 'Gnome', 'NORMAL', 'EARRINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('8', '14', '2', '4144', '1478', '1479', 'Tr', '1', '7', '15007', '1096', 'Troll', '121', '1', 'Troll', 'Troll', 'Troll', 'Trollesse', 'Troll', 'Troll', 'TUSKS', 'TUSKS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('9', '1', '1', '0', '6894', '6895', 'Go', '7', '7', '15007', '1096', 'Goblin', '0', '2', 'Goblin', 'Gobelin', 'Goblin', 'Gobeline', 'Goblin', 'Gobelin', 'NORMAL', 'NONE', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('10', '12', '2', '4142', '15476', '15475', 'Be', '1', '7', '15007', '1096', 'BloodElf', '162', '1', 'Blood Elf', 'Elfe de sang', 'Blood Elf', 'Elfe de sang', 'Blood Elf', 'Elfe de sang', 'NORMAL', 'EARRINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('11', '14', '1', '4140', '16125', '16126', 'Dr', '7', '7', '15007', '1096', 'Draenei', '163', '0', 'Draenei', 'Draeneï', 'Draenei', 'Draeneï', 'Draenei', 'Draeneï', 'NORMAL', 'HORNS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('12', '5', '1', '0', '16981', '16980', 'Fo', '7', '7', '15007', '1096', 'FelOrc', '0', '2', 'Fel Orc', 'Gangr\'orc', 'Fel Orc', 'Gangr\'orque', 'Fel Orc', 'Gangr\'orc', 'NORMAL', 'NORMAL', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('13', '1', '1', '0', '17402', '17403', 'Na', '7', '7', '15007', '1096', 'Naga_', '0', '2', 'Naga', 'Naga', 'Naga', 'Naga', 'Naga', 'Naga', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('14', '5', '1', '0', '17576', '17577', 'Br', '7', '7', '15007', '1096', 'Broken', '0', '2', 'Broken', 'Roué', 'Broken', 'Rouée', 'Broken', 'Roué', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('15', '1', '1', '0', '17578', '17579', 'Sk', '7', '7', '15007', '1096', 'Skeleton', '0', '2', 'Skeleton', 'Squelette', 'Skeleton', 'Squelette', 'Skeleton', 'Squelette', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('16', '1', '1', '0', '21685', '21686', 'Vr', '7', '7', '15007', '1096', 'Vrykul', '0', '2', 'Vrykul', 'Vrykul', 'Vrykul', 'Vrykule', 'Vrykul', 'Vrykul', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('17', '1', '1', '0', '21780', '21781', 'Tu', '7', '7', '15007', '1096', 'Tuskarr', '0', '2', 'Tuskarr', 'Rohart', 'Tuskarr', 'Rohart', 'Tuskarr', 'Rohart', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('18', '5', '1', '0', '21963', '21964', 'Ft', '7', '7', '15007', '1096', 'ForestTroll', '0', '2', 'Forest Troll', 'Troll des forêts', 'Forest Troll', 'Trollesse des forêts', 'Forest Troll', 'Troll des forêts', 'TUSKS', 'TUSKS', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('19', '5', '1', '0', '26316', '26317', 'Wt', '7', '7', '15007', '1096', 'Taunka', '0', '2', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Taunka', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('20', '5', '1', '0', '26871', '26872', 'NS', '7', '7', '15007', '1096', 'NorthrendSkeleton', '0', '2', 'Northrend Skeleton', 'Squelette du Norfendre', 'Northrend Skeleton', 'Squelette du Norfendre', 'Northrend Skeleton', 'Squelette du Norfendre', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('21', '5', '1', '0', '26873', '26874', 'It', '7', '7', '15007', '1096', 'IceTroll', '0', '2', 'Ice Troll', 'Troll des glaces', 'Ice Troll', 'Trollesse des glaces', 'Ice Troll', 'Troll des glaces', 'Normal', 'Normal', 'Normal', '0');
+INSERT INTO `chrracesdbc` VALUES ('22', '5', '2203', '20683', '29422', '29423', 'Wo', '7', '7', '15007', '1096', 'Worgen', '0', '2', 'Worgen', 'Worgen', 'Worgen', 'Worgen', 'Worgen', 'Worgen', 'NORMAL', 'EARS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('23', '5', '1', '4140', '49', '50', 'Hu', '7', '7', '15007', '1096', 'Human', '0', '2', 'Gilnean', 'Gilnéen', 'Gilnean', 'Gilnéenne', 'Gilnean', 'Gilnéen', 'NORMAL', 'PIERCINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('24', '5', '2395', '4140', '38551', '38552', 'Pa', '42', '7', '15007', '1096', 'Pandaren', '0', '2', 'Pandaren', 'Pandaren', 'Pandaren', 'Pandarène', 'Pandaren', 'Pandaren', 'NORMAL', 'EARRINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('25', '5', '2401', '4140', '38551', '38552', 'Pa', '7', '7', '15007', '1096', 'Pandaren', '0', '2', 'Pandaren', 'Pandaren', 'Pandaren', 'Pandarène', 'Pandaren', 'Pandaren', 'NORMAL', 'EARRINGS', 'NORMAL', '0');
+INSERT INTO `chrracesdbc` VALUES ('26', '5', '2402', '4140', '38551', '38552', 'Pa', '1', '7', '15007', '1096', 'Pandaren', '0', '2', 'Pandaren', 'Pandaren', 'Pandaren', 'Pandarène', 'Pandaren', 'Pandaren', 'NORMAL', 'EARRINGS', 'NORMAL', '0');
