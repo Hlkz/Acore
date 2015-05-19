d2a828d2980f180cb5e746c77b0129d08ad409da
Rat <gmstreetrat@gmail.com>
2015-05-19 11:22:07 +0100

    Core/GameObjects: Implemented gameobject_addon table, you can now set invisibility for gameobjects for quests
    (cherry picked from commit 9d59d038f8b09ed036448e755cb0b102396a4ca1)
    
    Conflicts:
    	sql/updates/world/2015_04_05_07_world.sql
    	src/server/game/Globals/ObjectMgr.cpp
    	src/server/game/Globals/ObjectMgr.h
    
    Core/GameObjects: fixed typo and logic (0 is a valid invisibility type)
    
    (cherry picked from commit 23e8a3ce2928458649d94408d5deffb67339b1d6)

--- src/server/game/Globals/ObjectMgr.h
+++ src/server/game/Globals/ObjectMgr.h
@@ -732,6 +732,7 @@ class ObjectMgr
         static void ChooseCreatureFlags(CreatureTemplate const* cinfo, uint32& npcflag, uint32& unit_flags, uint32& dynamicflags, CreatureData const* data = NULL);
         EquipmentInfo const* GetEquipmentInfo(uint32 entry, int8& id);
         CreatureAddon const* GetCreatureAddon(uint32 lowguid);
+        GameObjectAddon const* GetGameObjectAddon(ObjectGuid::LowType lowguid);
         CreatureAddon const* GetCreatureTemplateAddon(uint32 entry);
         ItemTemplate const* GetItemTemplate(uint32 entry);
         ItemTemplateContainer const* GetItemTemplateStore() const { return &_itemTemplateStore; }
@@ -961,6 +962,7 @@ class ObjectMgr
         void LoadLinkedRespawn();
         bool SetCreatureLinkedRespawn(uint32 guid, uint32 linkedGuid);
         void LoadCreatureAddons();
+        void LoadGameObjectAddons();
         void LoadCreatureModelInfo();
         void LoadEquipmentTemplates();
         void LoadGameObjectLocales();
@@ -1409,6 +1411,7 @@ class ObjectMgr
         CreatureModelContainer _creatureModelStore;
         CreatureAddonContainer _creatureAddonStore;
         CreatureAddonContainer _creatureTemplateAddonStore;
+        GameObjectAddonContainer _gameObjectAddonStore;
         EquipmentInfoContainer _equipmentInfoStore;
         LinkedRespawnContainer _linkedRespawnStore;
         CreatureLocaleContainer _creatureLocaleStore;
