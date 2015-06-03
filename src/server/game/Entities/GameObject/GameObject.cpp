94e9369b0e486c058fcd07280f96ce873924c4e8
Shauren <shauren.trinity@gmail.com>
2015-06-03 19:53:28 +0200

    Core/GameObject: Removed unneeded argument from GameObjectModel::Relocate and renamed it to UpdatePosition
    (cherry picked from commit 6e9ab38fcc9d2859393e452ef8630ee420783e47)

--- src/server/game/Entities/GameObject/GameObject.cpp
+++ src/server/game/Entities/GameObject/GameObject.cpp
@@ -2326,7 +2326,7 @@ void GameObject::UpdateModelPosition()
     if (GetMap()->ContainsGameObjectModel(*m_model))
     {
         GetMap()->RemoveGameObjectModel(*m_model);
-        m_model->Relocate(*this);
+        m_model->UpdatePosition();
         GetMap()->InsertGameObjectModel(*m_model);
     }
 }
